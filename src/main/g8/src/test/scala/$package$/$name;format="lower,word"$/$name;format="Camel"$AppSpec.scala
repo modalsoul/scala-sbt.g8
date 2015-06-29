c6aa5390a0a0016be2a2259cc52c8f14621403c0
modal_soul <modalsoul0215@gmail.com>
2015-06-29 22:01:16 +0900

    Update $name;format="Camel"$AppSpec.scala

--- src/main/g8/src/test/scala/$package$/$name;format="lower,word"$/$name;format="Camel"$AppSpec.scala
+++ src/main/g8/src/test/scala/$package$/$name;format="lower,word"$/$name;format="Camel"$AppSpec.scala
@@ -3,7 +3,7 @@ package $package$.$name;format="lower,word"$
 import org.scalatest.{FlatSpec, Matchers}
 
 object $name;format="Camel,word"$AppSpec extends FlatSpec with Matchers {
-  "foo" should "foo" {
+  "foo" should "foo" in {
     assert("foo" == "foo")
   }
 }
