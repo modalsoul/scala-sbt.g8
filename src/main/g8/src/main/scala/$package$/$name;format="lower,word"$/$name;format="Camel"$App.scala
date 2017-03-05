50ad352e9b1c514d9dd7569b657b44dc7c8f5538
imae <m-imae@biglobe.co.jp>
2017-03-05 21:59:44 +0900

    fix default object name

--- src/main/g8/src/main/scala/$package$/$name;format="lower,word"$/$name;format="Camel"$App.scala
+++ src/main/g8/src/main/scala/$package$/$name;format="lower,word"$/$name;format="Camel"$App.scala
@@ -1,5 +1,5 @@
 package $package$.$name;format="lower,word"$
 
-object $name;format="Camel,word"$App extends App {
+object $name;format="Camel"$App extends App {
   println("Hello, $name$")
 }
--- src/main/g8/src/test/scala/$package$/$name;format="lower,word"$/$name;format="Camel"$AppSpec.scala
+++ src/main/g8/src/test/scala/$package$/$name;format="lower,word"$/$name;format="Camel"$AppSpec.scala
@@ -2,7 +2,7 @@ package $package$.$name;format="lower,word"$
 
 import org.scalatest.{FlatSpec, Matchers}
 
-class $name;format="Camel,word"$AppSpec extends FlatSpec with Matchers {
+class $name;format="Camel"$AppSpec extends FlatSpec with Matchers {
   "foo" should "foo" in {
     assert("foo" == "foo")
   }
